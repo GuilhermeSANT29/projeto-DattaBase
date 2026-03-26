48642b6e798d3266fdb43a610ef826dc388ef640
Guilherme Santos <contate.guilherme.santos@gmail.com>
2026-03-26 10:32:16 -0300

    Add files via upload
    menu quase funcionando erros em sensores, manutenção e operadores, conexao ok - senha atual = senha123 login ADM@localhost / bash python -m pip install mysql-connector-python -bash C:/Users/Aluno/AppData/Local/Programs/Python/Python312/python.exe /

---
+++ projetodb/MySQL/projetoDB.sql
@@ -0,0 +1,187 @@
+CREATE DATABASE  IF NOT EXISTS `projetodb` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `projetodb`;
+-- MySQL dump 10.13  Distrib 8.0.44, for Win64 (x86_64)
+--
+-- Host: localhost    Database: projetodb
+-- ------------------------------------------------------
+-- Server version	8.0.44
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `alertas`
+--
+
+DROP TABLE IF EXISTS `alertas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `alertas` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `leituras_id` int DEFAULT NULL,
+  `classificacao` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `leituras_id` (`leituras_id`),
+  CONSTRAINT `alertas_ibfk_1` FOREIGN KEY (`leituras_id`) REFERENCES `leituras` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `alertas`
+--
+
+LOCK TABLES `alertas` WRITE;
+/*!40000 ALTER TABLE `alertas` DISABLE KEYS */;
+/*!40000 ALTER TABLE `alertas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `leituras`
+--
+
+DROP TABLE IF EXISTS `leituras`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `leituras` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `sensor_id` int DEFAULT NULL,
+  `valor` float DEFAULT NULL,
+  `data_hora` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `sensor_id` (`sensor_id`),
+  CONSTRAINT `leituras_ibfk_1` FOREIGN KEY (`sensor_id`) REFERENCES `sensores` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `leituras`
+--
+
+LOCK TABLES `leituras` WRITE;
+/*!40000 ALTER TABLE `leituras` DISABLE KEYS */;
+/*!40000 ALTER TABLE `leituras` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `manutencoes`
+--
+
+DROP TABLE IF EXISTS `manutencoes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `manutencoes` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `maquina_id` int DEFAULT NULL,
+  `operador_id` int DEFAULT NULL,
+  `descricao` text,
+  `data` date DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `maquina_id` (`maquina_id`),
+  KEY `operador_id` (`operador_id`),
+  CONSTRAINT `manutencoes_ibfk_1` FOREIGN KEY (`maquina_id`) REFERENCES `maquinas` (`id`),
+  CONSTRAINT `manutencoes_ibfk_2` FOREIGN KEY (`operador_id`) REFERENCES `operadores` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `manutencoes`
+--
+
+LOCK TABLES `manutencoes` WRITE;
+/*!40000 ALTER TABLE `manutencoes` DISABLE KEYS */;
+/*!40000 ALTER TABLE `manutencoes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `maquinas`
+--
+
+DROP TABLE IF EXISTS `maquinas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `maquinas` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) DEFAULT NULL,
+  `localizacao` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `maquinas`
+--
+
+LOCK TABLES `maquinas` WRITE;
+/*!40000 ALTER TABLE `maquinas` DISABLE KEYS */;
+INSERT INTO `maquinas` VALUES (1,'desktop',NULL),(2,'computador',NULL),(3,'esteira',NULL);
+/*!40000 ALTER TABLE `maquinas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `operadores`
+--
+
+DROP TABLE IF EXISTS `operadores`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `operadores` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) DEFAULT NULL,
+  `turno` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `operadores`
+--
+
+LOCK TABLES `operadores` WRITE;
+/*!40000 ALTER TABLE `operadores` DISABLE KEYS */;
+/*!40000 ALTER TABLE `operadores` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sensores`
+--
+
+DROP TABLE IF EXISTS `sensores`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `sensores` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `tipo` varchar(50) DEFAULT NULL,
+  `maquina_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `maquina_id` (`maquina_id`),
+  CONSTRAINT `sensores_ibfk_1` FOREIGN KEY (`maquina_id`) REFERENCES `maquinas` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sensores`
+--
+
+LOCK TABLES `sensores` WRITE;
+/*!40000 ALTER TABLE `sensores` DISABLE KEYS */;
+/*!40000 ALTER TABLE `sensores` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-03-26 10:15:30
